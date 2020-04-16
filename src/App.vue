b8a2415be929c00d3967d872a93898667f8686ca
jogarcia <jogarcia@uclv.cu>
2020-04-16 12:24:57 -0400

    Import number stepper for testing

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <number-stepper
+      v-model="someNumber"
+      show-number
+      :min="1"
+      :max="3"
+      label="Some label"
+      :allow-number-editing="false"
+    ></number-stepper>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NumberStepper from "./components/NumberStepper";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    NumberStepper
+  },
+
+  data: () => ({
+    someNumber: 1
+  })
+};
+</script>
